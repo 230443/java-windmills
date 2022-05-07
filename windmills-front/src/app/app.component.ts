1a1126af37782e10918611091a58601aece277b9
igorfstefanski <i.stefanski.00@gmail.com>
2022-05-07 22:47:12 +0200

    Add mockup and start the frontend

--- windmills-front/src/app/app.component.ts
+++ windmills-front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'windmills-front';
+  title = 'Java Windmills Project 2021/2022';
 }
